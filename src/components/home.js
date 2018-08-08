cc44f33d74bab038f088bfdd245824248f075721
Ethan Morrill <ethanmorrill@Ethans-MacBook-Pro.local>
2018-08-08 08:42:43 -0600

    Built home books component and installed redux form

--- src/components/home.js
+++ src/components/home.js
@@ -4,11 +4,25 @@ import Header from './header';
 export default class Home extends Component {
   render() {
     return (
-        <div className="home">
-          <Header className="home__header" small={false} />
-          <input className="home__search" type="text" name="search" placeholder='&#xf002; Search'></input>
-          home
+      <div className="home">
+        <Header className="home__header" small={false} />
+        <input className="home__search" type="text" name="search" placeholder='&#xf002; Search'></input>
+        <div className="home__books">
+          <div className="home__books__book">Book One</div>
+          <div className="home__books__book">Book Two</div>
+          <div className="home__books__book">Book Three</div>
+          <div className="home__books__book">Book Four</div>
+          <div className="home__books__book">Book Five</div>
+          <div className="home__books__book">Book Six</div>
+          <div className="home__books__book">Book Seven</div>
+          <div className="home__books__book">Book Eight</div>
+          <div className="home__books__book">Book Nine</div>
+          <div className="home__books__book">Book Ten</div>
+          <div className="home__books__book">Book Eleven</div>
+          <div className="home__books__book">Book Twelve</div>
+          <div className="home__books__book">Book Thirteen</div>
         </div>
+      </div>
     )
   }
 }
