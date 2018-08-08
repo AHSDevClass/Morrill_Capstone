7a8b6869f9d9dbff84b6c2a864e71f42cc1e1032
Ethan Morrill <ethanmorrill@Ethans-MacBook-Pro.local>
2018-08-08 09:34:48 -0600

    More of my sister's work

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormTitle } from './formTitle';
+import { FormInput } from './formFields';
 
 class LoginForm extends Component {
   render() {
     return (
       <form className="login-form">
         <FormTitle className="login-form__title" text="Login" />
+        <Field 
+          className="login-form__email" 
+          component={FormInput} name="email" 
+          type="email" 
+        />
       </form>
     )
   }
